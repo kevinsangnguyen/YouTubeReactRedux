2f74a01d8145573ce187172883a1739867b6dced
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-06-19 15:29:51 -0700

    added API_KEY from Google Youtube API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'; //render react components
 import ReactDOM from 'react-dom'; //render components to dom
 
+const API_KEY = 'AIzaSyDlVBzGQx0BtzwU7n5-sdsJRYBUM7jdGoA';
+
 // Create a new component. This componeent should produce some HTML
 const App = () => {  // CLASS BUT NOT INSTANCE
   return <div>Hi!</div>;
