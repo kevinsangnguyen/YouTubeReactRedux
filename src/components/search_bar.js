dcd40e41865e31a43cb35375e9de9ab5c92d4bbf
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-06-19 16:11:52 -0700

    search bar added as class component

---
+++ src/components/search_bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+// const SearchBar = () => {  //functional component
+//   return <input /> // React.createElement
+// };
+
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'; //render react components
 import ReactDOM from 'react-dom'; //render components to dom
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDlVBzGQx0BtzwU7n5-sdsJRYBUM7jdGoA';
 
 // Create a new component. This componeent should produce some HTML
 const App = () => {  // CLASS BUT NOT INSTANCE
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
