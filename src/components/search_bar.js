1332e8d1355b0a7fabe3ae4a6990476eb1be04bc
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-06-20 17:57:21 -0700

    added consutrctor and state variables

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,14 +6,21 @@ import React, { Component } from 'react';
 // };
 
 class SearchBar extends Component {
-  render() {
-    return <input onChange={(event) => console.log(event.target.value)} />; //render some jsx
+  constructor(props){
+    super(props);
+
+    this.state = { term: ''};
+    //STATE IS A PLAIN JAVASCRIPT OBJECT THAT IS USED TO RECORD/REACT USER EVENTS
   }
 
-  //EVENT HANDLER
-  onInputChange(event){
-    console.log(event.target.value);
+  render() {
+    return <input onChange={event => this.setState({ term: event.target.value })} />; //render some jsx
   }
+
+  // //EVENT HANDLER
+  // onInputChange(event){
+  //   console.log(event.target.value);
+  // }
 }
 
 export default SearchBar;
