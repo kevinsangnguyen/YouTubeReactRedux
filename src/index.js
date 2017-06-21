50442ea1ade3c095354af38d39e148e0a97b2878
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-06-20 20:59:53 -0700

    finalize youtube search

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react'; //render react components
 import ReactDOM from 'react-dom'; //render components to dom
 import YTSearch from 'youtube-api-search';
@@ -18,7 +19,11 @@ class App extends Component {  // CLASS BUT NOT INSTANCE
       selectedVideo: null,
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('kobe bryant')
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0],
@@ -27,9 +32,11 @@ class App extends Component {  // CLASS BUT NOT INSTANCE
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300) // returns a new function called every 300ms
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
