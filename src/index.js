6d01bd6e2dcd7eb248fe245c8709991ea1b770ed
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-06-20 20:00:24 -0700

    added videolist and details and player

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'; //render react components
 import ReactDOM from 'react-dom'; //render components to dom
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDlVBzGQx0BtzwU7n5-sdsJRYBUM7jdGoA';
 
@@ -11,10 +13,16 @@ class App extends Component {  // CLASS BUT NOT INSTANCE
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
     });
   }
 
@@ -22,6 +30,8 @@ class App extends Component {  // CLASS BUT NOT INSTANCE
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
